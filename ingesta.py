d7893baa9f1eb66c8e575233b85fe765527d7d12
JFpro160 <jose.paca@utec.edu.pe>
2024-10-05 00:26:06 -0500

    jpaca bucket

--- ingesta.py
+++ ingesta.py
@@ -1,7 +1,7 @@
 import boto3
 
 ficheroUpload = "data.csv"
-nombreBucket = "jpaca/ingesta/"
+nombreBucket = "jpaca"
 
 s3 = boto3.client('s3')
 response = s3.upload_file(ficheroUpload, nombreBucket, ficheroUpload)
